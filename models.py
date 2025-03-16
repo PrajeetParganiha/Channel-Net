3204400960532dec8ae5756384cc4aafd303b2aa
PrajeetParganiha <155004007+PrajeetParganiha@users.noreply.github.com>
2025-03-16 13:14:25 +0530

    Update models.py

--- models.py
+++ models.py
@@ -28,59 +28,49 @@ def psnr(target, ref):
 
     return 20 * math.log10(255. / rmse)
 
-def interpolation(noisy , SNR , Number_of_pilot , interp):
-    noisy_image = np.zeros((40000,72,14,2))
+def interpolation(noisy, SNR, Number_of_pilot, interp):
+    num_samples, H, W = noisy.shape  # Get dataset size dynamically
 
-    noisy_image[:,:,:,0] = np.real(noisy)
-    noisy_image[:,:,:,1] = np.imag(noisy)
+    # Create a 4D array for real & imaginary parts
+    noisy_image = np.zeros((num_samples, H, W, 2))
+    noisy_image[:, :, :, 0] = np.real(noisy)
+    noisy_image[:, :, :, 1] = np.imag(noisy)
 
-
-    if (Number_of_pilot == 48):
-        idx = [14*i for i in range(1, 72,6)]+[4+14*(i) for i in range(4, 72,6)]+[7+14*(i) for i in range(1, 72,6)]+[11+14*(i) for i in range(4, 72,6)]
-    elif (Number_of_pilot == 16):
-        idx= [4+14*(i) for i in range(1, 72,9)]+[9+14*(i) for i in range(4, 72,9)]
-    elif (Number_of_pilot == 24):
-        idx = [14*i for i in range(1,72,9)]+ [6+14*i for i in range(4,72,9)]+ [11+14*i for i in range(1,72,9)]
-    elif (Number_of_pilot == 8):
-      idx = [4+14*(i) for  i in range(5,72,18)]+[9+14*(i) for i in range(8,72,18)]
-    elif (Number_of_pilot == 36):
-      idx = [14*(i) for  i in range(1,72,6)]+[6+14*(i) for i in range(4,72,6)] + [11+14*i for i in range(1,72,6)]
-
-
-
-    r = [x//14 for x in idx]
-    c = [x%14 for x in idx]
-
-
-
-    interp_noisy = np.zeros((40000,72,14,2))
-
-    for i in range(len(noisy)):
-        z = [noisy_image[i,j,k,0] for j,k in zip(r,c)]
-        if(interp == 'rbf'):
-            f = interpolate.Rbf(np.array(r).astype(float), np.array(c).astype(float), z,function='gaussian')
-            X , Y = np.meshgrid(range(72),range(14))
-            z_intp = f(X, Y)
-            interp_noisy[i,:,:,0] = z_intp.T
-        elif(interp == 'spline'):
-            tck = interpolate.bisplrep(np.array(r).astype(float), np.array(c).astype(float), z)
-            z_intp = interpolate.bisplev(range(72),range(14),tck)
-            interp_noisy[i,:,:,0] = z_intp
-        z = [noisy_image[i,j,k,1] for j,k in zip(r,c)]
-        if(interp == 'rbf'):
-            f = interpolate.Rbf(np.array(r).astype(float), np.array(c).astype(float), z,function='gaussian')
-            X , Y = np.meshgrid(range(72),range(14))
-            z_intp = f(X, Y)
-            interp_noisy[i,:,:,1] = z_intp.T
-        elif(interp == 'spline'):
-            tck = interpolate.bisplrep(np.array(r).astype(float), np.array(c).astype(float), z)
-            z_intp = interpolate.bisplev(range(72),range(14),tck)
-            interp_noisy[i,:,:,1] = z_intp
-
-
-    interp_noisy = np.concatenate((interp_noisy[:,:,:,0], interp_noisy[:,:,:,1]), axis=0).reshape(80000, 72, 14, 1)
-   
+    # Define pilot indices
+    pilot_indices = {
+        48:  [14*i for i in range(1, 72,6)] + [4+14*i for i in range(4, 72,6)] + 
+             [7+14*i for i in range(1, 72,6)] + [11+14*i for i in range(4, 72,6)],
+        16:  [4+14*i for i in range(1, 72,9)] + [9+14*i for i in range(4, 72,9)],
+        24:  [14*i for i in range(1, 72,9)] + [6+14*i for i in range(4, 72,9)] + [11+14*i for i in range(1, 72,9)],
+        8:   [4+14*i for i in range(5, 72, 18)] + [9+14*i for i in range(8, 72, 18)],
+        36:  [14*i for i in range(1, 72,6)] + [6+14*i for i in range(4, 72,6)] + [11+14*i for i in range(1, 72,6)]
+    }
     
+    if Number_of_pilot not in pilot_indices:
+        raise ValueError("Unsupported number of pilots!")
+
+    idx = pilot_indices[Number_of_pilot]
+    r, c = np.array(idx) // 14, np.array(idx) % 14  # Convert 1D indices to 2D row-column
+
+    interp_noisy = np.zeros((num_samples, H, W, 2))
+
+    # Vectorized interpolation for all samples
+    X, Y = np.meshgrid(np.arange(H), np.arange(W), indexing="ij")  # Create grid
+
+    for i in range(2):  # Iterate over real (0) and imaginary (1) parts
+        z = noisy_image[:, r, c, i]  # Get pilot values for all samples
+        
+        if interp == 'rbf':
+            f = [interpolate.Rbf(r, c, z[j], function='gaussian') for j in range(num_samples)]
+            interp_noisy[:, :, :, i] = np.array([f[j](X, Y).T for j in range(num_samples)])
+
+        elif interp == 'spline':
+            tck = [interpolate.bisplrep(r, c, z[j]) for j in range(num_samples)]
+            interp_noisy[:, :, :, i] = np.array([interpolate.bisplev(np.arange(H), np.arange(W), tck[j]) for j in range(num_samples)])
+
+    # Reshape output as required
+    interp_noisy = np.concatenate((interp_noisy[:, :, :, 0], interp_noisy[:, :, :, 1]), axis=0).reshape(2 * num_samples, H, W, 1)
+
     return interp_noisy
 
 def SRCNN_model():
