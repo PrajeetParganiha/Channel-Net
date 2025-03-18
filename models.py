76f9e883d6e4acf8216a21f24510590977d2fa6d
PrajeetParganiha <155004007+PrajeetParganiha@users.noreply.github.com>
2025-03-18 21:31:45 +0530

    Update models.py

--- models.py
+++ models.py
@@ -105,7 +105,7 @@ def SRCNN_train(train_data ,train_label, val_data , val_label , channel_model , 
     callbacks_list = [checkpoint]
 
     srcnn_model.fit(train_data, train_label, batch_size=128, validation_data=(val_data, val_label),
-                    callbacks=callbacks_list, shuffle=True, epochs= 1 , verbose=0)
+                    callbacks=callbacks_list, shuffle=True, epochs= 50 , verbose=0)
     
     #srcnn_model.save_weights("drive/codes/my_srcnn/SRCNN_SUI5_weights/SRCNN_48_12.h5")
     srcnn_model.save_weights("SRCNN_" + channel_model +"_"+ str(num_pilots) + "_"  + str(SNR) + ".weights.h5")
@@ -148,7 +148,7 @@ def DNCNN_train(train_data ,train_label, val_data , val_label, channel_model , n
   callbacks_list = [checkpoint]
 
   dncnn_model.fit(train_data, train_label, batch_size=128, validation_data=(val_data, val_label),
-                  callbacks=callbacks_list, shuffle=True, epochs= 1, verbose=0)
+                  callbacks=callbacks_list, shuffle=True, epochs= 50, verbose=0)
   dncnn_model.save_weights("DNCNN_" + channel_model +"_"+ str(num_pilots) + "_"  + str(SNR) + ".weights.h5")
   
   
